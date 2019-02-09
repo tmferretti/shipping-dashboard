28d91655bebe006072a4abe22618f3ee0198c558
tmferretti <michaelf@ritani.com>
2019-02-09 18:35:11 -0500

    Add latitude and longitude columns and index to addresses table

---
+++ db/migrate/20190209233352_add_latitude_and_longitude_columns_to_addresses.rb
@@ -0,0 +1,7 @@
+class AddLatitudeAndLongitudeColumnsToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :addresses, :latitude, :decimal
+    add_column :addresses, :longitude, :decimal
+    add_index :addresses, [:latitude, :longitude]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_220103) do
+ActiveRecord::Schema.define(version: 2019_02_09_233352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2019_02_09_220103) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.index ["latitude", "longitude"], name: "index_addresses_on_latitude_and_longitude"
   end
 
   create_table "line_items", force: :cascade do |t|
