549650c59af04e65e10b344ab574fb3d6ba9c328
tmferretti <michaelf@ritani.com>
2019-02-09 17:05:46 -0500

    Add shipment_address join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_214649) do
+ActiveRecord::Schema.define(version: 2019_02_09_220103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2019_02_09_214649) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "vendor_id"
+  end
+
+  create_table "shipment_addresses", force: :cascade do |t|
+    t.integer "address_id"
+    t.integer "shipment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shipments", force: :cascade do |t|
