9d4875a13490e511ce3b41f330cecc9f8a6795ff
tmferretti <michaelf@ritani.com>
2019-02-09 15:32:44 -0500

    Add basic v1 versioning to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.michaelferretti.com; version=1"}) do
+      # match '/foos.(:format)' => 'foos#index', :via => :get
+      # match '/foos_no_format' => 'foos#index', :via => :get
+      # resources :bars
+    end
+  end
 end
