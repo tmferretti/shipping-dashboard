27475de390aa475a0c050a36f25be5d35b99dc9d
tmferretti <michaelf@ritani.com>
2019-02-09 18:43:25 -0500

    Include geocoding in Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,12 @@ class Address < ApplicationRecord
   belongs_to :vendor
   has_many :shipment_addresses
   has_many :shipments, through: :shipment_addresses
+
+  geocoded_by :pretty_address
+
+  # after_validation :geocode # for after initial seed
+
+  def pretty_address
+    "#{address1}, #{address2 if address2}, #{city}, #{state}, #{country}"
+  end
 end
