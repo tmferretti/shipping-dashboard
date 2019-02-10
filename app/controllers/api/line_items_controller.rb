506965949f35054bd49ddc6aa36683e2c1e26360
tmferretti <michaelf@ritani.com>
2019-02-10 00:04:26 -0500

    Add empty crud actions to resource controllers

--- app/controllers/api/line_items_controller.rb
+++ app/controllers/api/line_items_controller.rb
@@ -1,4 +1,18 @@
 module api
   class LineItemsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
   end
 end
--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -1,4 +1,18 @@
 module api
   class ShipmentsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
   end
 end
