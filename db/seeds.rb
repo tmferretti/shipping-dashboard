ca39aaa630a772cf0236cc20057de2408c150d71
tmferretti <michaelf@ritani.com>
2019-02-09 21:02:12 -0500

    Delete all rows of models that are to be instantiated in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Delete prexisting data in tables. Delete wont run any callbacks so its much faster, given this seeds
+# file is populating tables with alot of rows, sure. Typically I will use destroy in production code 
+# outside of a seeds file.
+Vendor.delete_all
+Address.delete_all
+Order.delete_all
+LineItem.delete_all
+Shipment.delete_all
