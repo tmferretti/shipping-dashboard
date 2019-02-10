ea75779df439fcac6b7f80d3235835788f3fecbf
tmferretti <michaelf@ritani.com>
2019-02-09 20:59:37 -0500

    Add vendor foreign key to addresses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_233352) do
+ActiveRecord::Schema.define(version: 2019_02_10_015905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_02_09_233352) do
     t.datetime "updated_at", null: false
     t.decimal "latitude"
     t.decimal "longitude"
+    t.integer "vendor_id"
     t.index ["latitude", "longitude"], name: "index_addresses_on_latitude_and_longitude"
   end
 
