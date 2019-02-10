01ed7ab97c3579aae1d370d97aa4592d9ed133ac
tmferretti <michaelf@ritani.com>
2019-02-09 23:57:58 -0500

    Add CRUD actions to addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,23 @@
-class AddressesController < ApplicationController
+module api
+  class AddressesController < ApplicationController
+    def index
+      @addresses = Address.all
+    end
+
+    def show
+      @address = Address.find_by(id: params[:id])
+    end
+
+    def update
+    end
+
+    def create
+    end
+
+    def destroy
+      address = Address.find_by(id: params[:id])
+      address.active = false
+      address.save
+    end
+  end
 end
