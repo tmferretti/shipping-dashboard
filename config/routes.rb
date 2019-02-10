a6315479a3f2ba4e165d35ef09517fb54d10ac03
tmferretti <michaelf@ritani.com>
2019-02-09 23:48:20 -0500

    Add shallow routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
-    api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.michaelferretti.com; version=1"}) do
-      # match '/foos.(:format)' => 'foos#index', :via => :get
-      # match '/foos_no_format' => 'foos#index', :via => :get
-      # resources :bars
+    api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.negotiatus-challenge.com; version=1"}) do
+      shallow do
+        resources :vendors do
+          resources :orders do
+            resources :line_items
+            resources :shipments do
+              resources :addresses
+            end
+          end
+
+          resources :addresses, :only => [:create, :index]
+        end
+      end
     end
   end
 end
