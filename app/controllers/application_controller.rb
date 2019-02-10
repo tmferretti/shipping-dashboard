900d753cafe74cf2a953bb2518f20b068805aedc
tmferretti <michaelf@ritani.com>
2019-02-10 00:32:59 -0500

    Add expose method to application controller to assit with loading instance variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def self.expose(variable_name)
+    define_method(variable_name) do
+      @exposed_variables ||= {}
+      @exposed_variables[variable_name] ||= yield
+    end
+    helper_method variable_name
+  end
 end
