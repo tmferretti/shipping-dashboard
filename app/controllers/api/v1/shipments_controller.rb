0a4905b8939d10875668ca2e2e19517e48699374
tmferretti <michaelf@ritani.com>
2019-02-10 19:19:32 -0500

    Refactor Shipments controller to use more readable instance definition

--- app/controllers/api/v1/shipments_controller.rb
+++ app/controllers/api/v1/shipments_controller.rb
@@ -1,15 +1,13 @@
 module Api
   module V1
     class ShipmentsController < ApplicationController
-      expose(:shipments) { Shipment.where(vendor_id: params[:vendor_id]) } if params[:vendor_id]
-      expose(:shipments) { Shipment.where(order_id: params[:order_id]) } if params[:order_id]
-      expose(:shipment) { Shipment.where(id: params[:id]) }
-
       def index
+        @shipments = find_shipments(order_id: params[:order_id], vendor_id: params[:vendor_id])
         render 'index.json.jbuilder'
       end
 
       def show
+        @shipment = find_shipment(params[:id])
         render 'show.json.jbuilder'
       end
 
@@ -21,6 +19,24 @@ module Api
 
       def destroy
         @shipment.destroy
+      end
+
+      private
+
+      def find_shipment(id)
+        Shipment.find(id)
+      end
+
+      def find_shipments(order_id: nil, vendor_id: nil)
+        # since both vendors and orders have shipments, we share the index route between them,
+        # normally, this would introduce some unnecessary complexity into the view, however this is an api
+        # so its not a problem
+
+        if order_id
+          Shipment.where(order_id: order_id)
+        else
+          Shipment.where(vendor_id: vendor_id)
+        end
       end
     end
   end
